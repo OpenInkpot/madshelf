d8896fea1116810d5e82113e0aba1175d45df5c5
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-01-11 06:48:29 +0600

    Use eoi_battery instead of copy-pasted code

--- src/battery.c
+++ src/battery.c
@@ -1,7 +1,7 @@
 /*
  * MadShelf - bookshelf application.
  *
- * Copyright (C) 2009 Mikhail Gusarov <dottedmag@dottedmag.net>
+ * Copyright (C) 2010 Mikhail Gusarov <dottedmag@dottedmag.net>
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -18,78 +18,11 @@
  * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <unistd.h>
-#include <stdio.h>
-
-#include <Edje.h>
+#include <libeoi_battery.h>
 
 #include "battery.h"
 
-typedef struct
-{
-    char* now;
-    char* min;
-    char* max;
-} battery_info_t;
-
-static battery_info_t batteries[] =
-{
-    {
-        "/sys/class/power_supply/n516-battery/charge_now",
-        "/sys/class/power_supply/n516-battery/charge_empty_design",
-        "/sys/class/power_supply/n516-battery/charge_full_design",
-    },
-    {
-        "/sys/class/power_supply/lbookv3_battery/charge_now",
-        "/sys/class/power_supply/lbookv3_battery/charge_empty_design",
-        "/sys/class/power_supply/lbookv3_battery/charge_full_design",
-    },
-};
-
-static int _find_battery()
-{
-    int i;
-    for(i = 0; i < sizeof(batteries)/sizeof(batteries[0]); ++i)
-    {
-        if(!access(batteries[i].now, R_OK))
-            return i;
-    }
-    return -1;
-}
-
-static int _read_int_file(const char* filename)
-{
-    int res = 0;
-    FILE* f = fopen(filename, "r");
-    fscanf(f, "%d", &res);
-    fclose(f);
-    return res;
-}
-
-/*
- * -1 - unknown
- * 0..100 - charge
- */
-static int _get_state()
-{
-    int batt = _find_battery();
-
-    if(batt == -1)
-        return -1;
-
-    int now = _read_int_file(batteries[batt].now);
-    int min = _read_int_file(batteries[batt].min);
-    int max = _read_int_file(batteries[batt].max);
-
-    return 100 * (now - min) / (max - min);
-}
-
 void update_battery(Evas_Object* top)
 {
-    int charge = _get_state();
-
-    char signal[256];
-    snprintf(signal, 256, "battery-level,%d", charge);
-
-    edje_object_signal_emit(top, signal, "");
+    eoi_draw_battery_info(top);
 }
