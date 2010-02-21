437a47b02fa8f5b7010316942450b81c8e875dc3
Mikhail Gusarov <dottedmag@dottedmag.net>
2010-02-22 02:10:00 +0600

    Use ecore_strbuf instead of open-written string buffer

--- src/text_escape.c
+++ src/text_escape.c
@@ -19,61 +19,25 @@
  */
 #include "text_escape.h"
 
-#include <string.h>
-#include <malloc.h>
-#include <Evas.h>
+#include <Ecore_Data.h>
 
-static char *
-_str_append(char *str, const char *txt, int *len, int *alloc)
+char *
+textblock_escape_string(const char *text)
 {
-    int txt_len = strlen(txt);
+    if (!text)
+        return NULL;
 
-    if (txt_len <= 0) return str;
-    if ((*len + txt_len) >= *alloc)
-    {
-        char *str2;
-        int alloc2;
+    Ecore_Strbuf *buf = ecore_strbuf_new();
+    ecore_strbuf_append(buf, text);
 
-        alloc2 = *alloc + txt_len + 128;
-        str2 = realloc(str, alloc2);
-        if (!str2) return str;
-        *alloc = alloc2;
-        str = str2;
-    }
-    strcpy(str + *len, txt);
-    *len += txt_len;
-    return str;
-}
+    ecore_strbuf_replace_all(buf, "\n", "<br>");
+    ecore_strbuf_replace_all(buf, "\t", "<\t>");
+    ecore_strbuf_replace_all(buf, "<", "&lt;");
+    ecore_strbuf_replace_all(buf, ">", "&gt;");
+    ecore_strbuf_replace_all(buf, "&", "&amp;");
 
-char* textblock_escape_string(const char *text)
-{
-    char *str = strdup("");
-    int str_len = 0, str_alloc = 0;
-    int ch, pos = 0, pos2 = 0;
+    char *res = strdup(ecore_strbuf_string_get(buf));
+    ecore_strbuf_free(buf);
 
-    if (!text) return NULL;
-    for (;;)
-    {
-        pos = pos2;
-        pos2 = evas_string_char_next_get(text, pos2, &ch);
-        if ((ch <= 0) || (pos2 <= 0)) break;
-        if (ch == '\n')
-            str = _str_append(str, "<br>", &str_len, &str_alloc);
-        else if (ch == '\t')
-            str = _str_append(str, "<\t>", &str_len, &str_alloc);
-        else if (ch == '<')
-            str = _str_append(str, "&lt;", &str_len, &str_alloc);
-        else if (ch == '>')
-            str = _str_append(str, "&gt;", &str_len, &str_alloc);
-        else if (ch == '&')
-            str = _str_append(str, "&amp;", &str_len, &str_alloc);
-        else
-        {
-            char tstr[16];
-            strncpy(tstr, text + pos, pos2 - pos);
-            tstr[pos2 - pos] = 0;
-            str = _str_append(str, tstr, &str_len, &str_alloc);
-        }
-    }
-    return str;
+    return res;
 }
